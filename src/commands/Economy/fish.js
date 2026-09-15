8d652172dfb8bb3042a0bdf07f848a4e30173b89
spets994-alt <spets994@gmail.com>
2026-09-15 07:08:54 -0400

    fish.js

--- src/commands/Economy/fish.js
+++ src/commands/Economy/fish.js
@@ -4,7 +4,7 @@ import { getEconomyData, setEconomyData } from '../../utils/economy.js';
 import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHandler.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const FISH_COOLDOWN = 0* 1 * 1000; 
+const FISH_COOLDOWN = 44* 60* 1000; 
 const BASE_MIN_REWARD = 300;
 const BASE_MAX_REWARD = 900;
 const FISHING_ROD_MULTIPLIER = 1.5;
